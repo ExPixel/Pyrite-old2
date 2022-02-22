566f94be3ee1c9d8dff510b23e39ff1bd3c78367
Adolph C <adolphc@outlook.com>
2022-02-21 22:39:05 -0500

    Add unit test for scheduler.

--- gba/src/scheduler.rs
+++ gba/src/scheduler.rs
@@ -22,6 +22,13 @@ impl Scheduler {
     pub fn advance(&mut self, cycles: impl Into<Cycles>) -> Option<(EventFn, Cycles)> {
         self.inner.borrow_mut().advance(cycles.into())
     }
+
+    #[cfg(test)]
+    pub fn dump(&self) {
+        for (idx, event) in self.inner.borrow().events.iter().enumerate() {
+            println!("EVENT{}: {} cycles", idx, event.cycles_remaining);
+        }
+    }
 }
 
 pub struct Event {
@@ -63,12 +70,53 @@ impl Inner {
     }
 }
 
-    fn advance(&mut self, cycles: Cycles) -> Option<(EventFn, Cycles)> {
-        if self.has_next_event(cycles) {
-            let event = self.events.pop_front().unwrap();
-            Some((event.callback, cycles - event.cycles_remaining))
-        } else {
-            None
+#[cfg(test)]
+mod test {
+    use arm::Cycles;
+
+    use crate::Gba;
+
+    use super::Scheduler;
+
+    #[test]
+    fn basic_scheduling() {
+        let mut gba = Gba::default();
+        let mut scheduler = Scheduler::default();
+
+        scheduler.schedule(|_, _| data()[0] = 1, 10u32);
+        scheduler.schedule(|_, _| data()[3] = 1, 17u32);
+        scheduler.schedule(|_, _| data()[1] = 1, 13u32);
+        scheduler.schedule(|_, _| data()[2] = 1, 13u32);
+        scheduler.dump();
+
+        assert!(scheduler.advance(6u32).is_none());
+        assert_eq!(data()[0], 0);
+
+        let (cb, cycles) = scheduler.advance(6u32).expect("expected event");
+        assert_eq!(cycles, Cycles::from(2u32));
+        cb(&mut gba, cycles);
+        assert!(scheduler.advance(cycles).is_none());
+        assert_eq!(*data(), [1, 0, 0, 0]);
+
+        let (cb, cycles) = scheduler.advance(Cycles::ONE).expect("expected event");
+        assert_eq!(cycles, Cycles::ZERO);
+        cb(&mut gba, cycles);
+        assert_eq!(*data(), [1, 1, 0, 0]);
+        let (cb, cycles) = scheduler.advance(cycles).expect("expected event");
+        assert_eq!(cycles, Cycles::ZERO);
+        cb(&mut gba, cycles);
+        assert!(scheduler.advance(cycles).is_none());
+        assert_eq!(*data(), [1, 1, 1, 0]);
+
+        let (cb, cycles) = scheduler.advance(8).expect("expected event");
+        assert_eq!(cycles, Cycles::from(4u32));
+        cb(&mut gba, cycles);
+        assert!(scheduler.advance(cycles).is_none());
+        assert_eq!(*data(), [1, 1, 1, 1]);
+
+        static mut DATA: [u32; 4] = [0; 4];
+        fn data() -> &'static mut [u32; 4] {
+            unsafe { &mut DATA }
         }
     }
 }

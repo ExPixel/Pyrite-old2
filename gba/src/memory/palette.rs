6d63d80a78fa06e979769d8cc38240afce810498
Adolph C <adolphc@outlook.com>
2022-03-10 17:14:46 -0500

    Box palette in GbaMemory and unbox palette data.
    I realized that Palette is usually passed around behind a reference
    anyway so it doesn't make sense to box
    its contents.

--- gba/src/memory/palette.rs
+++ gba/src/memory/palette.rs
@@ -1,18 +1,15 @@
-use util::{
-    array,
-    mem::{read_u16, read_u32, write_u16, write_u32},
-};
+use util::mem::{read_u16, read_u32, write_u16, write_u32};
 
 use super::{PAL_MASK, PAL_SIZE};
 
 pub struct Palette {
-    pub(crate) data: Box<[u8; PAL_SIZE as usize]>,
+    pub(crate) data: [u8; PAL_SIZE as usize],
 }
 
 impl Default for Palette {
     fn default() -> Self {
         Palette {
-            data: array::boxed_copied(0),
+            data: [0; PAL_SIZE as usize],
         }
     }
 }
@@ -36,11 +33,11 @@ impl Palette {
     }
 
     pub fn load32(&self, address: u32) -> u32 {
-        read_u32(&*self.data, (address & PAL_MASK) as usize)
+        read_u32(&self.data, (address & PAL_MASK) as usize)
     }
 
     pub fn load16(&self, address: u32) -> u16 {
-        read_u16(&*self.data, (address & PAL_MASK) as usize)
+        read_u16(&self.data, (address & PAL_MASK) as usize)
     }
 
     pub fn load8(&self, address: u32) -> u8 {
@@ -48,11 +45,11 @@ impl Palette {
     }
 
     pub fn store32(&mut self, address: u32, value: u32) {
-        write_u32(&mut *self.data, (address & PAL_MASK) as usize, value);
+        write_u32(&mut self.data, (address & PAL_MASK) as usize, value);
     }
 
     pub fn store16(&mut self, address: u32, value: u16) {
-        write_u16(&mut *self.data, (address & PAL_MASK) as usize, value);
+        write_u16(&mut self.data, (address & PAL_MASK) as usize, value);
     }
 
     pub fn store8(&mut self, address: u32, value: u8) {
@@ -64,11 +61,11 @@ impl Palette {
     }
 
     pub fn view32(&self, address: u32) -> u32 {
-        read_u32(&*self.data, (address & PAL_MASK) as usize)
+        read_u32(&self.data, (address & PAL_MASK) as usize)
     }
 
     pub fn view16(&self, address: u32) -> u16 {
-        read_u16(&*self.data, (address & PAL_MASK) as usize)
+        read_u16(&self.data, (address & PAL_MASK) as usize)
     }
 
     pub fn view8(&self, address: u32) -> u8 {

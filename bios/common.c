bdb33410055ad592e77e515cdf9b096df13b69d4
Adolph C <adolphc@outlook.com>
2022-03-12 21:25:21 -0500

    More bios work.
    For some reason this doesn't work correctly if common.c is compiled with optimizations :(

--- bios/common.c
+++ bios/common.c
@@ -1,11 +1,45 @@
 #include <stddef.h>
 
-void *memset(void *dst, int value, unsigned int count)
+/* Add the compiler optimization to inhibit loop transformation to library
+   calls.  This is used to avoid recursive calls in memset and memmove
+   default implementations.  */
+#ifdef HAVE_CC_INHIBIT_LOOP_TO_LIBCALL
+#define inhibit_loop_to_libcall \
+    __attribute__((__optimize__("-fno-tree-loop-distribute-patterns")))
+#else
+#define inhibit_loop_to_libcall
+#endif
+
+inhibit_loop_to_libcall void *memset(void *dest, int val, size_t len)
 {
-    unsigned char *ptr = (unsigned char *)dst;
-    while (count-- > 0)
+    unsigned char *ptr = dest;
+    while (len-- > 0)
+        *ptr++ = val;
+    return dest;
+}
+
+inhibit_loop_to_libcall void *memcpy(void *dest, const void *src, size_t len)
+{
+    char *d = dest;
+    const char *s = src;
+    while (len--)
+        *d++ = *s++;
+    return dest;
+}
+
+inhibit_loop_to_libcall void *memmove(void *dest, const void *src, size_t len)
+{
+    char *d = dest;
+    const char *s = src;
+    if (d < s)
+        while (len--)
+            *d++ = *s++;
+    else
     {
-        *ptr = (unsigned char)value;
+        char *lasts = (char *)s + (len - 1);
+        char *lastd = d + (len - 1);
+        while (len--)
+            *lastd-- = *lasts--;
     }
-    return dst;
+    return dest;
 }

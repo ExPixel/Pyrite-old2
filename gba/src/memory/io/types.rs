6960743daaac62be4b65123c51784392512e8187
Adolph C <adolphc@outlook.com>
2022-03-22 01:34:07 -0400

    Rename sound to audio (to match video)

--- gba/src/memory/io/types.rs
+++ gba/src/memory/io/types.rs
@@ -1,13 +1,13 @@
+mod audio;
 mod dma;
 mod interrupts;
 mod lcd;
-mod sound;
 mod system;
 mod timers;
 
+pub use audio::*;
 pub use dma::*;
 pub use interrupts::*;
 pub use lcd::*;
-pub use sound::*;
 pub use system::*;
 pub use timers::*;

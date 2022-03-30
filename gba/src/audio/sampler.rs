2f4b2ca6be1beaa373ed36ce3a815d657774ffd3
Adolph C <adolphc@outlook.com>
2022-03-30 17:58:41 -0400

    More accurate timing for sample output.

--- gba/src/audio/sampler.rs
+++ gba/src/audio/sampler.rs
@@ -3,35 +3,33 @@ use crate::memory::io::FifoChannel;
 use super::Command;
 
 pub struct GbaAudioSampler {
-    native_frequency: u32,
-    frequency: u32,
+    native_frequency: f64,
     fifo_a: f32,
     fifo_b: f32,
-    wait_frames: u32,
+    wait_frames: f64,
 }
 
 impl GbaAudioSampler {
     pub fn new(native_frequency: u32) -> Self {
-        assert!(native_frequency.is_power_of_two());
+        // assert!(native_frequency.is_power_of_two());
         GbaAudioSampler {
-            native_frequency,
-            frequency: 32768,
+            native_frequency: native_frequency as f64,
             fifo_a: 0.0,
             fifo_b: 0.0,
-            wait_frames: 0,
+            wait_frames: 0.0,
         }
     }
 
     pub fn frame(&mut self) -> (f32, f32) {
-        self.wait_frames = self.wait_frames.saturating_sub(1);
+        if self.wait_frames >= 0.0 {
+            self.wait_frames -= 1.0;
+        }
         (self.fifo_a, self.fifo_a)
     }
 
     fn wait_cycles(&mut self, cycles: u32) {
-        const GBA_FREQ: u64 = 16 * 1024 * 1024;
-        let wait_frames = (cycles as u64 * self.frequency as u64) / GBA_FREQ;
-        self.wait_frames =
-            ((wait_frames * self.native_frequency as u64) / self.frequency as u64) as u32;
+        const GBA_FREQ: f64 = 1.0 / (16.0 * 1024.0 * 1024.0);
+        self.wait_frames += (cycles as f64 * self.native_frequency) * GBA_FREQ;
     }
 
     pub fn command(&mut self, command: Command) {
@@ -48,13 +46,13 @@ impl GbaAudioSampler {
                 }
             }
             Command::SetResolution(resolution) => {
-                self.frequency = resolution.frequency();
-                log::debug!("sampler frequency changed to {}", self.frequency);
+                // FIXME: This is currently ignored. Should it continue to be this way?
+                log::debug!("GBA frequency changed to {}", resolution.frequency());
             }
         }
     }
 
     pub fn needs_commands(&self) -> bool {
-        self.wait_frames == 0
+        self.wait_frames <= 1.0
     }
 }
